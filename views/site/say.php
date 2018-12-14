875dca35c69b73b17b5b45e4045645a0c89272d5
relesssar <45258081+relesssar@users.noreply.github.com>
2018-12-14 16:09:50 +0600

    hello

---
+++ views/site/say.php
@@ -0,0 +1,9 @@
+<?php
+use yii\helpers\Html;
+$this->title = 'Say';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<h1> <?= Html::encode($this->title) ?> </h1>
+<h1>Hello <?= Html::encode($target) ?> </h1>
+<p>Welcome</p>
